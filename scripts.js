7045fa506883dbb9a08e9ba6e4da2c931554fb33
LeonMesquita <87508711+LeonMesquita@users.noreply.github.com>
2022-04-11 13:07:18 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -33,9 +33,9 @@ function startMatch(){
 
     do{
         cardsNumber = prompt("Informe com quantas cartas você quer jogar")
-    } while ( cardsNumber < 4 || cardsNumber > 14 || cardsNumber % 2 !== 0)
+    } while (cardsNumber % 2 !== 0)
 
-
+// cardsNumber < 4 || cardsNumber > 14 || 
 
 }
 
