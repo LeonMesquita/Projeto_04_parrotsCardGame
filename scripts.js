33575396e9a0e2a9e8ca5b83ebbb29f5f0f03e46
LeonMesquita <87508711+LeonMesquita@users.noreply.github.com>
2022-04-11 15:09:52 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
-
-
 let cardsNumber = 0
 let totalTurnedCards = 0
 let totalOfPlays = 0
-let time = 0
+let minutes = 00
+let seconds = 00
 let idInterval
 let cardsList = []
 let turnedCards = 0
@@ -28,15 +27,15 @@ function startMatch(){
     cardsNumber = 0
     totalTurnedCards = 0
     totalOfPlays = 0
-    time = 0
+    minutes = 0
+    seconds = 0
+    previousCard = ""
+    currentCard = ""
     cardsList = []
 
     do{
         cardsNumber = prompt("Informe com quantas cartas você quer jogar")
-    } while (cardsNumber % 2 !== 0)
-
-// cardsNumber < 4 || cardsNumber > 14 || 
-
+    } while (cardsNumber < 4 || cardsNumber > 14 || cardsNumber % 2 !== 0)
 }
 
 startMatch()
@@ -75,16 +74,21 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
+
+
 setCardsList()
 
+
+
+// Exibindo os cards na tela
 
 const cards = document.querySelector(".cards-list")
 
 function showCards(){
+    
     for (let cont = 0; cont < cardsList.length; cont++){
         cards.innerHTML += `<div class="card" onclick="turnCard(this, ${cont})">
-
-        <div class="front-face face">
+        <div class="face">
             <img src= "images/front 1.png">
         </div>             
         </div>`
@@ -99,17 +103,12 @@ showCards()
 let card1Index
 let card2Index
 
+//Virar um card ao clicar
+
 function turnCard(card, index){
     let path = cardsList[index].cardPath
-    if (turnedCards < 2){
- 
-
+    if (turnedCards < 2 && (previousCard === "" || currentCard === "")){
         card.querySelector("img").src = path
-        card.querySelector("div").classList.toggle('front-face')
-
-
-        cardsElements.push(card)
-
         turnedCards++
         totalOfPlays++
         if (turnedCards === 1){
@@ -130,59 +129,78 @@ function turnCard(card, index){
     }
 
     if (totalTurnedCards === cardsList.length){
-        alert(`Você ganhou em ${totalOfPlays} jogadas!`)
-        let answer = prompt("deseja reiniciar a partida?")
-        if (answer === "sim"){
+        clearInterval(idInterval)
+        alert(`Você ganhou em ${totalOfPlays} jogadas e seu tempo foi ${minutes} minutos e ${seconds} segundos!`)
+        let answer
+        while (answer !== "sim" && answer != "não"){
+            answer = prompt("deseja reiniciar a partida?")
+            if (answer === "sim"){
             cards.innerHTML = ""
             startMatch()
             setCardsList()
             showCards()
-
-
-            
         }
+        }
+
     }
 
     
     
 
 }
+	
+//Verificar se os cards são iguais
 
 function checkPlay(){
     if (card1Path === card2Path && currentCard !== previousCard){
         totalTurnedCards += 2
         previousCard.onclick=""
         currentCard.onclick=""
+        previousCard = ""
+        currentCard = ""
     }
     else {
         setTimeout(flipCards, 1000)
     }
 }
-//
 
+
+//Virar os cards de volta
 
 function flipCards(){
-    previousCard.querySelector("div").classList.toggle('front-face')
-    currentCard.querySelector("div").classList.toggle('front-face')
     previousCard.querySelector("img").src = "images/front 1.png"
     currentCard.querySelector("img").src = "images/front 1.png"
+    previousCard = ""
+    currentCard = ""
 }
 
 
 function showTime(){
-    
-    document.querySelector(".clock").innerHTML = time
 
+   
     idInterval = setInterval(increment, 1000)
 }
 
 function increment() {
-    time++
-    document.querySelector(".clock").innerHTML = time
-
-    if (totalTurnedCards === cardsList.length){
-        clearInterval(idInterval)
+    seconds++
+    s = "0"
+    m = "0"
+    if (seconds > 9){
+        s = ""
     }
+    if (minutes > 9){
+        m = ""
+    }
+    if (seconds === 60){
+        minutes++
+        seconds = 0
+    }
+    
+    document.querySelector(".clock").innerHTML = `
+    <img src="images/alarm-outline.svg" />
+
+    <h1>${m}${minutes}:${s}${seconds}</h1>`
+
+
 
 }
-
